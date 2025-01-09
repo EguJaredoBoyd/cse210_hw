d2b4c69b1fd1777635411e0d1c1df74d1c5256f9
EguJaredoBoyd <eguchidieberejared@gmail.com>
2025-01-09 14:17:01 -0800

    Initial

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+        Console.WriteLine("What is your first name? ");
+        string Firstname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string Lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Lastname}, {Firstname} {Lastname}.");
+        
+
     }
 }
