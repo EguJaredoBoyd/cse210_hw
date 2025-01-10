4fd63bfc01645b47428281f3ee53cf8c75565360
EguJaredoBoyd <eguchidieberejared@gmail.com>
2025-01-10 13:54:01 -0800

    Added a Semicolon

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.")
+        Console.WriteLine("Hello World! This is the Exercise2 Project.");
         bool playAgain = true;
 
         while (playAgain)
